01629fe730b45e923663823272df6ac2fb02b4f2
Fabien <fabien.lenoir@gmail.com>
2017-05-28 00:49:24 +0200

    prod rdy

---
+++ src/map/SupporterCounter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Animate } from 'react-move';
+import { connect } from 'react-redux';
+
+
+const SupporterCounter = ({ markersCount, likesCount, animationDuration }) => (
+  <div>
+    <p>Encouragez Brian et rejoignez les:</p>
+    <Animate
+      data={{ n: markersCount + likesCount }}
+      duration={animationDuration}
+      easing="easeSinOut"
+    >
+      {data => (<div style={{ color: '#26B8D0', paddingTop: 5, fontSize: 35 }}>
+        {Math.round(data.n).toLocaleString('fr', { maximumFractionDigits: 2 }) }
+      </div>)}
+    </Animate>
+    <p style={{ textTransform: 'uppercase', paddingTop: 10, color: '#5F6061', fontSize: 20 }}>supporters</p>
+  </div>
+);
+
+export default connect(({ likesCount, animationDuration }) => ({
+  likesCount, animationDuration,
+}))(SupporterCounter);
